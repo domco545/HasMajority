22ff55704d93c1d03af431c74b6a5654f455009b
Dominik Rothbauer <dominikrothbauer@gmail.com>
2020-09-16 18:42:28 +0200

    Initial commit

---
+++ HasMajority/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace HasMajority
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var program = new Program();
+            int[] arr2 = program.FillArray();
+            int[] arr = { 5, 4, 2, 4, 4};
+            program.HasMajority(arr2);
+            watch.Stop();
+            Console.WriteLine($"Execute time was {watch.ElapsedMilliseconds} miliseconds that is {watch.ElapsedMilliseconds / 1000} seconds");
+        }
+
+        public int[] FillArray() 
+        {
+            var rand = new Random();
+            int[] arr = new int[1000];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(1,100);
+            }
+            return arr;
+        }
+
+        public bool HasMajority(int[] arr) 
+        {
+            var lenght = arr.Length;
+            var currentNum = 0;
+            var count = -1;
+            var percentKeeper = 1;
+
+            foreach (var value in arr)
+            {
+                currentNum = value;
+                Console.WriteLine($"counting {currentNum}");
+
+                count = countMember(currentNum, arr);
+                Console.WriteLine($"done totoal apperances is {count}");
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                var percent = (percentKeeper * 100) / lenght;
+                Console.WriteLine($"finished {percent}%");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                         
+                if (count > lenght/2)
+                {
+                    Console.WriteLine($"majority number is {currentNum} and appeared {count} times");
+                    return true;
+                }
+
+                percentKeeper++;
+            }
+
+            Console.WriteLine("no majority number");
+            return false;
+        }
+
+        private int countMember(int num, int[] arr) 
+        {
+            var count = 0;
+            foreach (var item in arr)
+            {
+                if (item == num)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ HasMajorityFaster/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HasMajorityFaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var program = new Program();
+            int[] arr2 = program.FillArray();
+            int[] arr = { 5, 4, 2, 4, 4 };
+            program.HasMajority(arr2);
+            watch.Stop();
+            Console.WriteLine($"Execute time was {watch.ElapsedMilliseconds} miliseconds that is {watch.ElapsedMilliseconds / 1000} seconds");
+        }
+
+        public int[] FillArray()
+        {
+            var rand = new Random();
+            int[] arr = new int[100000000];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(1, 100);
+            }
+            return arr;
+        }
+
+        public bool HasMajority(int[] arr) 
+        {
+            Lookup<int, int> lookup = (Lookup<int, int>)arr.ToLookup(x => x, x=> x);
+
+            foreach (var x in lookup)
+            {
+                var count = x.Count();
+                Console.WriteLine($"Found {x.Key}, {count} times");
+                if (count > arr.Length/2)
+                {
+                    Console.WriteLine($"Found majority {x.Key} presented {count} times");
+                    return true;
+                }
+            }
+
+            Console.WriteLine("no majority");
+            return false;
+        }
+    }
+}
